4a9ab291ed61a2e6b927c1bf29b3132aca4a6403
lucaslmeireles <86924068+lucaslmeireles@users.noreply.github.com>
2024-04-27 00:39:32 +0000

    starting product page

---
+++ src/components/groups/dropdownmenuproductview.tsx
@@ -0,0 +1,25 @@
+import { MoreHorizontal } from "lucide-react";
+import { Button } from "../ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "../ui/dropdown-menu";
+
+export default function DropdownMenuProduct () {
+    return (
+        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                              <Button
+                                aria-haspopup="true"
+                                size="icon"
+                                variant="ghost"
+                              >
+                                <MoreHorizontal className="h-4 w-4" />
+                                <span className="sr-only">Toggle menu</span>
+                              </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                              <DropdownMenuItem>Edit</DropdownMenuItem>
+                              <DropdownMenuItem>Delete</DropdownMenuItem>
+                            </DropdownMenuContent>
+                          </DropdownMenu>
+    )
+}
